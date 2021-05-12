5037c0fd36d5ae52d69e14fc57f4c514a9c6a2f6
nazorm <nnadoziechinazor@gmail.com>
2021-05-12 07:22:58 +0100

    set up add record

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
 import React, { useEffect, useState } from "react";
 import RecordCard from "./RecordCard";
-import { Input, Button, Spin } from "antd";
+import { Spin } from "antd";
 import "./App.scss";
 import axios from "axios";
-const { Search } = Input;
 
 function App() {
   const [pharmaRecords, setPharmaRecords] = useState([]);
+  const [addedRecord, setAddedRecord] = useState("");
   const [loading, setLoading] = useState(true);
-  const addRecord = () => {
-    alert("record added");
+  const [count, setCount]= useState(15);
+const date = new Date().toLocaleDateString();
+  const handleAddedRecord = (e) => {
+    const { value } = e.target;
+    setAddedRecord(value);
   };
 
-  const handleDelete = (id)=>{
-    const filtered = pharmaRecords.filter((d)=>{
-      return d.id !== id
-    })
-    setPharmaRecords(filtered)
-  }
+  const addRecord = (e) => {
+    e.preventDefault();
+    setCount(c => c + 1)
+    const newRecord = {
+      id: count,
+      name: addedRecord,
+      prices: [
+        {
+          id: 1,
+          price: "",
+          date: date,
+        },
+        {
+          id: 2,
+          price: "",
+          date: "",
+        },
+      ],
+    };
+    const newRecordList = [newRecord, ...pharmaRecords];
+    setPharmaRecords(newRecordList);
+  };
+
+  const handleDelete = (id) => {
+    const filtered = pharmaRecords.filter((d) => {
+      return d.id !== id;
+    });
+    setPharmaRecords(filtered);
+  };
   useEffect(() => {
     const fetchRecords = async () => {
       setLoading(true);
       const res = await axios.get(
         "http://www.mocky.io/v2/5c3e15e63500006e003e9795"
       );
-      setPharmaRecords(res.data.products)
-      setLoading(false)
+      setPharmaRecords(res.data.products);
+      setLoading(false);
     };
     fetchRecords();
   }, []);
@@ -33,27 +59,30 @@ function App() {
     <div className="App">
       <h1>Pharma Records</h1>
       <form onSubmit={addRecord} className="addrecord-form">
-        <Search
-          placeholder="Add records here"
-          allowClear
-          enterButton="Add"
-          size="large"
-          onSearch={addRecord}
+        <input
+          type="text"
+          value={addedRecord}
+          placeholder="Add records here..."
+          className="add-input"
+          onChange={handleAddedRecord}
         />
+        <button className="add-btn">Add</button>
       </form>
-      {loading ? <Spin /> : 
-      <div className="record-container">
-        {pharmaRecords.map((record)=>{
-          return <RecordCard
-          key = {record.id}
-          recordCardData = {record}
-          handleDelete = {handleDelete}
-          />
-        })}
-        
+      {loading ? (
+        <Spin />
+      ) : (
+        <div className="record-container">
+          {pharmaRecords.map((record) => {
+            return (
+              <RecordCard
+                key={record.id}
+                recordCardData={record}
+                handleDelete={handleDelete}
+              />
+            );
+          })}
         </div>
-      
-      }
+      )}
     </div>
   );
 }
